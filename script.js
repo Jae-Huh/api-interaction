fd1383db8b723454af4d0b9e5cd1753a23e8be25
Jae Huh <jaeeunhuh@gmail.com>
2017-04-05 19:00:03 +1000

    Update javascript format

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ var url = 'https://eda-te-reo.herokuapp.com/api/whakatauki'
 function getProverb() {
   fetch(url)
     .then (function(res) {
-      res.json().then(function(data){
-        document.getElementById('maori').innerHTML = data.source
-        document.getElementById('english').innerHTML = data.translation
-      })
+      return res.json()
+    })
+    .then(function(data){
+      document.getElementById('maori').innerHTML = data.source
+      document.getElementById('english').innerHTML = data.translation
     })
 }
 
